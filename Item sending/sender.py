fd1410f236807b6b8cf27859a699c8df91866cd5
Eric Briese <ebrie3@gmail.com>
2015-12-30 16:23:57 +1000

    Fixing examples and configs and names.

--- Item sending/sender.py
+++ Item sending/sender.py
@@ -3,6 +3,6 @@ from Toribash_stuff.library.toripy import *
 session = login()
 
 counter = 0
-for name in open("receiving_gorman.txt", "r").read().splitlines():
+for name in open("send_to_list.txt", "r").read().splitlines():
 	sendItem(session, first_item_id_of_batch_buy + counter, name)
 	counter += 1
