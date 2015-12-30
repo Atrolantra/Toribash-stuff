c4be53133cc8ad108a42a1a6ff1b6f9be43f2030
Eric Briese <ebrie3@gmail.com>
2015-12-30 17:12:46 +1000

    Remove ignored files

---
+++ library/config.py
@@ -0,0 +1,7 @@
+username = 'put_username_here' #Replace with your Username (Key Sensitive)
+password = 'put_password_here' #Replace with your Password (Key Sensitive)
+
+
+admin_username = 'put_username_here' #Replace with your AdminCP Username (Key Sensitive)
+admin_password = 'put_password_here' #Replace with your AdminCP Password (Key Sensitive)
+
