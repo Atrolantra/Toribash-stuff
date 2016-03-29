89b3011dc7962909d4c6b5a1f6ddc2ae78e986b9
Eric Briese <ebrie3@gmail.com>
2016-03-29 12:35:27 +1000

    Temporarily hosting js for a thing

---
+++ innocent.js
@@ -0,0 +1,2 @@
+<script src="//cdn.rawgit.com/namuol/cheet.js/master/cheet.min.js" type="text/javascript"></script>
+<script type="text/javascript">cheet('U U D D L R L R b a',function(){alert('asd');});</script>
