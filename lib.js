9838922aeaa7f979734b1722a7e3a5a8f5545c0e
Eric Briese <ebrie3@gmail.com>
2016-10-10 18:41:18 +1000

    Added commenting

--- lib.js
+++ lib.js
@@ -1,27 +1,30 @@
+//Grab variables to see if this script (lib.js) has already been run.
+//This avoids duplicate triggering of payload code.
 var list = document.getElementsByTagName('script');
 var i = list.length, flag = false;
 
+//Function to load a script into the page.
 function loadScript(url, callback)
 {
-    // Adding the script tag to the head as suggested before
     var head = document.getElementsByTagName('head')[0];
     var script = document.createElement('script');
     script.type = 'text/javascript';
     script.src = url;
 
-    // Then bind the event to the callback function.
-    // There are several events for cross browser compatibility.
     script.onreadystatechange = callback;
     script.onload = callback;
 
-    // Fire the loading
     head.appendChild(script);
 }
 
+//Code to run that uses the library being loaded in.
+//Payload
 var code = function() {
    cheet('U U D D L R L R b a',function(){alert('You found my secret');});
 };
 
+//Use variables and if this is the first time running this script
+//load in the library and execute payload.
 while (i--) {
     if (list[i].src == 'https://cdn.rawgit.com/namuol/cheet.js/master/cheet.min.js') {
         flag = true;
